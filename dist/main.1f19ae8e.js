c21fd3d5f0282c1e0f792c42937a51fe0eec6d9f
Dav <southcaliappt@gmail.com>
2023-03-30 10:09:01 -0500

    build fix

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -37537,7 +37537,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54567" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62641" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
